eda1919da9d0274f21a1640eee9c090e043d8f7f
Martin Ziegler <martin.ziegler3398@gmail.com>
2023-11-08 18:05:37 +0900

    Added code to detect borders

--- licenseplates/lpmodel.py
+++ licenseplates/lpmodel.py
@@ -1,3 +1,4 @@
+import cv2
 import ultralytics
 
 
@@ -8,13 +9,36 @@ def load_model():
 
 
 def predict_bounding_box(model, image):
-    results = model(image)
+    results = model(image)[0]
 
-    return results
+    lp_boxes = [box_data for box_data in results.boxes.data.tolist() if box_data[5] == 0]
+    if not lp_boxes:
+        raise ValueError("No license plate found")
+
+    best_box = max(lp_boxes, key=lambda box: box[4])
+
+    return {
+        "bb": best_box[:4],
+        "cropped_img": image[int(best_box[1]) : int(best_box[3]), int(best_box[0]) : int(best_box[2])],
+        "confidence": best_box[4],
+    }
 
 
-def detect_borders(image, bounding_box):
-    pass
+def detect_borders(bounding_box_data):
+    cropped_img = bounding_box_data["cropped_img"]
+    gray_image = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2GRAY)
+
+    _, mask = cv2.threshold(gray_image, 100, 255, cv2.THRESH_BINARY)
+
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    contour = max(contours, key=cv2.contourArea)
+
+    rect = cv2.minAreaRect(contour)
+
+    box = cv2.boxPoints(rect).astype(int)
+
+    return box
 
 
 def transform_license_plate(image, borders):
@@ -27,8 +51,8 @@ def read_license_plate(image):
 
 def get_license_text(image):
     model = load_model()
-    bounding_box = predict_bounding_box(model, image)
-    borders = detect_borders(image, bounding_box)
+    bounding_box_data = predict_bounding_box(model, image)
+    borders = detect_borders(bounding_box_data)
     license_plate = transform_license_plate(image, borders)
     content = read_license_plate(license_plate)
 
--- prediction.py
+++ prediction.py
@@ -1,12 +1,22 @@
 import cv2
+from matplotlib import pyplot as plt
 
 from licenseplates import lpmodel
 
-EXAMPLE_IMAGE_PATH = "License-Plates-5/test/images/b1a50a3824887ee2_jpg.rf.68a4fd34fce20184287592f2680f895b.jpg"
+EXAMPLE_IMAGE_PATH = "License-Plates-5/test/images/bbcac63e32bd8137_jpg.rf.ef4704b0ada4fbbf613143abf52f6f86.jpg"
 
 if __name__ == "__main__":
-    model = lpmodel.load_model()
-
     test_image = cv2.imread(EXAMPLE_IMAGE_PATH)
-    text = lpmodel.predict_bounding_box(model, test_image)
-    print(text)
+
+    bounding_box_data = lpmodel.predict_bounding_box(lpmodel.load_model(), test_image)
+    x1, y1, x2, y2 = bounding_box_data["bb"]
+    bb_image = test_image.copy()
+    cv2.rectangle(bb_image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 1)
+    plt.imshow(bb_image)
+    plt.show()
+
+    border_edges = lpmodel.detect_borders(bounding_box_data)
+    border_image = bounding_box_data["cropped_img"].copy()
+    cv2.drawContours(border_image, [border_edges], 0, (0, 255, 0), 2)
+    plt.imshow(border_image)
+    plt.show()
