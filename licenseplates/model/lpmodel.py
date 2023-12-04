4cf2b57584d947dfbbe5d10018bad810ffdf2d68
Maximilian Muza <muza.maximilian@gmail.com>
2023-12-04 16:56:02 +0900

    Improve thresholding hyperparams

--- licenseplates/model/lpmodel.py
+++ licenseplates/model/lpmodel.py
@@ -98,16 +98,17 @@ def transform_license_plate(image, rectangle, corners):
 def read_license_plate(image):
     upscaled_image = cv2.resize(image, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
     gray = cv2.cvtColor(upscaled_image, cv2.COLOR_BGR2GRAY)
-    denoised_image = cv2.GaussianBlur(gray, (7, 7), 0)
+    denoised_image = cv2.GaussianBlur(gray, (3, 3), 0)
     _, otsu_thresholding = cv2.threshold(denoised_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    adaptive_thresholding = cv2.adaptiveThreshold(denoised_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 4)
+    adaptive_thresholding = cv2.adaptiveThreshold(denoised_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 15, 10)
+    adaptive_thresholding_2 = cv2.adaptiveThreshold(denoised_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 27, 9)
 
-    images_to_plot = [upscaled_image, gray, denoised_image, otsu_thresholding, adaptive_thresholding]
-    titles = ['Upscaled Image', 'Grayscale Image', 'Denoised Image', 'Otsu Thresholded Image', 'Adaptive Thresholded Image']
+    images_to_plot = [upscaled_image, gray, denoised_image, otsu_thresholding, adaptive_thresholding, adaptive_thresholding_2]
+    titles = ['Upscaled Image', 'Grayscale Image', 'Denoised Image', 'Otsu Thresholded Image', 'Adaptive Thresholded Image', 'Adaptive Thresholded Image 2']
 
-    # result = pytesseract.image_to_string(adaptive_thresholding, lang='eng')#
+    # result = pytesseract.image_to_string(adaptive_thresholding, lang='eng')
     reader = easyocr.Reader(['en'])
-    result = reader.readtext(adaptive_thresholding)
+    result = reader.readtext(adaptive_thresholding_2)
 
     return result, images_to_plot, titles
 
